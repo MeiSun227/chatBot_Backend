96300d853412f45d39526c0666f5f41490b012b3
meisun227 <meysunlee@gmail.com>
2024-02-21 15:53:04 +0200

    chatbot_endpoint

---
+++ server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+
+const answerJson = [
+    { answer: "Meow, you asked too much questions." },
+    { answer: "Meow. I don't answer question unless there are treats.." },
+    { answer: "I'm sorry, I didn't understand that." },
+    {answer:"Meow meow"},
+    {answer:"Meow meow meow"},
+    {answer:"Meow service is out of office, come back on Monday"},
+    { answer: "That's an interesting question." },
+    { answer: "I'm not sure how to answer that, meow." }
+];
+app.use(express.json());
+app.use(cors());
+const PORT = 3002;
+
+app.post('/ask', (req, res) => {
+    const randomIndex= Math.floor(Math.random()*answerJson.length);
+    const randomAnswer = answerJson[randomIndex].answer;
+    res.json({answer:randomAnswer});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
